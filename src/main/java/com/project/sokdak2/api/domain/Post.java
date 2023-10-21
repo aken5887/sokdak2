f287cb54665376dab312db605959fe0768dc8fee
aken <aken@kakao.com>
2023-10-21 21:56:52 +0900

    24. JPA 관련 수정

--- src/main/java/com/project/sokdak2/api/domain/Post.java
+++ src/main/java/com/project/sokdak2/api/domain/Post.java
@@ -4,7 +4,6 @@ import com.project.sokdak2.api.request.PostCreate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,9 +34,9 @@ public class Post extends BaseTimeEntity {
   private int password;
   @ColumnDefault("0")
   private Integer topFixed;
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "post")
+  @OneToMany(mappedBy = "post")
   private List<File> files = new ArrayList<>();
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "post")
+  @OneToMany(mappedBy = "post")
   private List<PostReply> replies = new ArrayList<>();
 
   @Builder
