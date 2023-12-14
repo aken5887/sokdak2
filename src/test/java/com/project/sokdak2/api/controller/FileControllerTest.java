4803eab68ba8dc2ce30b8ee3eb62b09a90e13b49
aken <aken@kakao.com>
2023-12-14 17:02:32 +0900

    29. File 다운로드 테스트 코드 추가 & css 수정 - 파일 다운로드 Exception 테스트 코드 추가 - 타이틀,nav 폰트 사이즈 변경

---
+++ src/test/java/com/project/sokdak2/api/controller/FileControllerTest.java
@@ -0,0 +1,35 @@
+package com.project.sokdak2.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class FileControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @DisplayName("/download 요청시 파일이 없는 경우에는 FileNotFoundException이 발생한다.")
+    @Test
+    void download_exception() throws Exception {
+        // expected
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/download/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/errors/error"))
+                .andExpect(model().attribute("error", hasProperty("code", is("404"))));
+    }
+
+}
