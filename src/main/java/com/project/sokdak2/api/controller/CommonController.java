fb064be283f366383108f0a07ff6326000944e9b
aken <aken@kakao.com>
2024-03-08 14:03:28 +0900

    delete resume.html

--- src/main/java/com/project/sokdak2/api/controller/CommonController.java
+++ src/main/java/com/project/sokdak2/api/controller/CommonController.java
@@ -1,23 +1,9 @@
 package com.project.sokdak2.api.controller;
 
-import com.project.sokdak2.api.domain.common.Visits;
-import com.project.sokdak2.api.repository.VisitsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class CommonController {
-    final VisitsRepository visitsRepository;
-    @GetMapping("/resume")
-    public String resume(Model model) {
-        List<Visits> visits
-                = visitsRepository.findAllByUriContaining("/resume");
-        model.addAttribute("vc", visits.size());
-        return "/common/resume";
-    }
 }
