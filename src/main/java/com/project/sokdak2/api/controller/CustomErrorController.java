a5d7d1f4cec3e96cd61cb7172ad7ea8f6a3ee61a
aken <aken@kakao.com>
2024-03-08 14:29:05 +0900

    [fixed] resume.html 삭제로 인한 html 수정

--- src/main/java/com/project/sokdak2/api/controller/CustomErrorController.java
+++ src/main/java/com/project/sokdak2/api/controller/CustomErrorController.java
@@ -4,34 +4,30 @@ import com.project.sokdak2.api.response.ErrorResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class CustomErrorController implements ErrorController {
     @GetMapping("/error")
     public String error(HttpServletRequest req, Model model){
-        Object statusCode = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        String code = "";
-        String message = "오류가 발생하였습니다.";
+        String statusCode
+                =  String.valueOf(req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+        String message = "";
 
-        if(statusCode != null){
-            if(String.valueOf(statusCode).startsWith("4")){
-                code = "4xx";
-                message = "잘못된 요청 입니다.";
-            }else{
-                code = "5xx";
-                message = "요청 중 오류가 발생하였습니다.";
-            }
+        if(statusCode.equals("404")){
+            message = "페이지가 존재 하지 않습니다.";
+        } else if(statusCode.startsWith("4")){
+            message = "잘못된 요청 입니다.";
+        } else {
+            message = "요청 중 오류가 발생하였습니다.";
         }
+
         ErrorResponse response
                 = ErrorResponse.builder()
-                .code(code)
+                .code(statusCode)
                 .message(message)
                 .build();
 
