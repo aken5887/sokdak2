4d73833c21aaa9587a5ebef1d3411ac94041294b
aken <aken@kakao.com>
2024-04-08 15:33:37 +0900

    [fiexed] spring boot 3 업데이트 이후 sqs listen 불가 현상

--- src/main/java/com/project/sokdak2/api/config/schedule/NewsScheduler.java
+++ src/main/java/com/project/sokdak2/api/config/schedule/NewsScheduler.java
@@ -7,27 +7,34 @@ import com.project.sokdak2.api.domain.post.Post;
 import com.project.sokdak2.api.repository.PostRepository;
 import com.project.sokdak2.api.request.MailMessage;
 import com.project.sokdak2.api.service.AwsSQSSender;
+import com.project.sokdak2.api.service.MailService;
 import com.project.sokdak2.api.service.NewsService;
+import com.project.sokdak2.api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class NewsScheduler {
     private final PostRepository postRepository;
     private final NewsService newsService;
     private final AwsSQSSender sqsSender;
-    @Scheduled(cron="${schedules.cron.newsArticle-get}")
+    private final MailService mailService;
+    private final UserService userService;
+
+    @Scheduled(cron = "${schedules.cron.newsArticle-get}")
     public void getNewsArticle() throws JsonProcessingException {
         String newsContent = newsService.getNewsArticleHtml();
-        String title = "["+LocalDate.now()+"] 오늘의 뉴스";
+        String title = "[" + LocalDate.now() + "] 오늘의 뉴스";
 
         List<Post> today = postRepository.findAllByTitle(title);
-        if(today.isEmpty()){
+        if (today.isEmpty()) {
             Post post = Post.builder()
                     .title(title)
                     .content(newsContent)
@@ -41,7 +48,14 @@ public class NewsScheduler {
                     .title(title)
                     .content(newsContent)
                     .build();
-            sqsSender.sendMessageSendMail(mailMessage);
+            // TODO spring boot 3 업데이트 이후 sqs listen 안되는 현상
+//            sqsSender.sendMessageSendMail(mailMessage);
+            List<String> emails = userService.findUserEmails();
+            for (String email : emails) {
+                log.info("Valid Email : {}", email);
+                mailMessage.updateEmail(email);
+                mailService.sendMail(mailMessage);
+            }
         }
     }
 }
